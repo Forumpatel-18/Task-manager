583752a0cfb7646ce477abc37d613755f66f1c72
Forum Patel <forum180301@gmail.com>
2023-04-18 05:49:19 +0530

    final commit

--- starter/app.js
+++ starter/app.js
@@ -18,7 +18,7 @@ app.use(notFound);
 
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async (req, res) => {
   try {
